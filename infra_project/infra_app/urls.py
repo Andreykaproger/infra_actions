9196110d6b251f12ec42575b3b6d8a38ae292383
Andreykaproger <korovalolka123456789@gmail.com>
2025-06-08 11:41:26 +0300

    last version

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,9 +1,9 @@
-import views
+from . import views
 from django.urls import path
 
 app_name = 'infra_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('second/', views.second_page, name='second_page'),
+    path('second_page/', views.second_page, name='second_page'),
 
 ]
